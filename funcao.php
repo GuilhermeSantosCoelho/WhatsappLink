fe92dfb698773acf975dd1931c20ee22d0463b29
Guilherme Santos Coelho <32603185+Skazybad13@users.noreply.github.com>
2018-12-18 21:26:23 -0200

    Add files via upload

---
+++ funcao.php
@@ -0,0 +1,23 @@
+<?php
+    /* Função PHP que retorna a URL encurtada a partir dos dados recebidos via POST */
+
+    $numero = $_POST['numero']; //Pega o número de telefone
+    $texto = $_POST['texto']; // Pega o texto da mensagem
+
+    $texto_novo = preg_replace('/[ -]+/' , '%20' , $texto); //Substitui os espaços da mensagem por %20 para a URL
+
+    $url = ("https://api.whatsapp.com/send?phone=".$numero."%26text=".$texto_novo); //Gera a URL da API do Whatsapp
+
+    $login = "skazybad"; //Seu login do Bit.ly
+    $api_key = "R_1841a82d7daa4a00b55c5cb9eef4355f"; //Seu token de acesso do Bit.ly
+
+    $ch = curl_init("http://api.bitly.com/v3/shorten?login=".$login."&apiKey=".$api_key."&longUrl=".$url);//Gera a URL da API do Bit.ly
+
+    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true); //Definindo a opção de cURL
+
+    $result = curl_exec($ch); //Obtendo o resultado da requisição
+
+    $res = json_decode($result,true); //Decodificando a resposta em JSON
+
+    echo $res['data']['url']; //Retornando somente a URL encurtada
+?>
